90bf885a3cdfaf6e5a62269e1befb8e35cf20071
cqupt-yifanwu <616584789@qq.com>
2016-12-14 10:24:26 +0800

    add ajax

--- page-plug.js
+++ page-plug.js
@@ -7,7 +7,8 @@
 	*		pageNav: DOMObject, // 含义：分页下导航的容器，里面的按钮是插件自动生成的，可以为其添加样式
 	*		ctsList: DOMObjects, // 含义：需要分页的总的内容的条数  注意：这里是数据确定的条件下需要的参数
 	*		prev: DOMobject, // 含义：上一页按钮
-	*		next: DOMobject // 含义：下一页按钮
+	*		next: DOMobject, // 含义：下一页按钮
+	*		url: url // 含义：请求数据的地址（非必需）
  	* }
 	*/
 
@@ -18,8 +19,30 @@
 		ctsList = obj.ctsList,
 		pageNav = obj.pageNav,
 		prev = obj.prev,
-		next = obj.next;
-		
+		next = obj.next
+		url = obj.url;
+
+		// 若数据需要从服务器获取，需要打开下面的注释，然后将第46行-100行放入37行的函数中（ajax的成功回调函数）
+		/*if (!ctsList) {
+			//*
+			//*  注意： 这里服务器发送的数据需要有一定的格式限制
+			//*  基本格式为 
+			//*	[{
+			//*		text: ...内容...
+			//*	},
+			//*	{
+			//*		text: ...内容...
+			//*	}]
+			//*
+			ajax(url, function (data) {
+				var html = '';
+				for (var i = 0, len = data.length; i < len; i++) {
+					html + = "<li>" + data[i].text + "</li>";
+				}
+				contentUl.innerHTML = html;
+			})
+		}*/
+
 		var ctsListChilds = ctsList.children;       	      // li元素集，保存到变量中，防止反复访问，提升性能
 		var pagers = pageNav.children;   					  // 每一个页号，保存到变量中，防止反复访问
 
@@ -75,4 +98,36 @@
 		}
 
 		showContains(1);
+
+		// 集成了原生ajax请求的函数
+		function ajax(url,fnSucc,fnFail){              //fnSucc是成功时的处理程序，fnFail是失败时的处理程序
+			//1.创建Ajax对象
+			if(window.XMLHttpRequest){
+				var oAjax=new XMLHttpRequest();
+			}
+			else{       //针对ie6
+				var oAjax=new ActiveXObject("Microsoft.XMLHTTP");  
+			}
+
+			//2.链接服务器   open 三个参数（方法，文件名，是否异步）
+			oAjax.open("GET",url,true);
+
+			//3.发送请求
+			oAjax.send();
+
+			//4.接受返回
+			oAjax.onreadystatechange=function(){
+				// oAjax.readyState 提供信息浏览器和服务器进行到哪一步了
+				if(oAjax.readyState==4){ //读取完成
+					if(oAjax.status==200){
+						fnSucc(oAjax.responseText);
+					}
+					else{
+						if(fnFail){       //此函数存在
+							fnFail(oAjax.status);
+						}
+					}
+				} 
+			}
+		};
 	}
